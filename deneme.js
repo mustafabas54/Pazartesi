c16e08f02d610e375eb79be58a189dc55a409d0b
mustafabas54 <57364655+mustafabas54@users.noreply.github.com>
2019-11-26 01:33:42 +0300

    Add files via upload

---
+++ deneme.js
@@ -0,0 +1,22 @@
+let hamburger = document.getElementById('hamburger')
+let lines = document.getElementsByClassName('line')
+let cc = document.getElementById('circle-container')
+let cember = document.getElementById('cember')
+let logo = document.getElementById('resim')
+
+const dondur = ()=> {
+    cember.classList.add('yeni')
+}
+const geri = ()=> {
+    cember.classList.remove('yeni')
+}
+setInterval(dondur, 1000)
+setInterval(geri, 2000)
+
+hamburger.addEventListener('click', () =>{
+    lines[0].classList.toggle('active-line1')
+    lines[1].classList.toggle('active-line2')
+    lines[2].classList.toggle('active-line3')
+    
+
+})
